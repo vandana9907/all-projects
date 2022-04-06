59721bec4f79d355501dd8849459a80bf90f8491
vandana <vandanagurram07@gmail.com>
2022-04-06 22:42:30 +0530

    add the registration

--- travel/src/app/app.module.ts
+++ travel/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
