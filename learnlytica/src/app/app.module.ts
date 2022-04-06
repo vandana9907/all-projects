118a3284d5e499e03b74f41f1c040491dd654257
vandana <vandanagurram07@gmail.com>
2022-04-06 12:28:18 +0530

    main container

---
+++ learnlytica/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { ListCourseComponent } from './components/list-course/list-course.component';
+import { CourseComponent } from './components/course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContainerComponent,
+    ListCourseComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ learnlytica/src/app/components/course/course.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+courseTitle ='git for begginers';
+courseDate = new Date()
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
