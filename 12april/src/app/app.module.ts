76312f93df27daf380bfba57da03ffbf492b67f1
vandana <vandanagurram07@gmail.com>
2022-04-12 18:41:29 +0530

    angular

--- 12april/src/app/app.module.ts
+++ 12april/src/app/app.module.ts
@@ -4,25 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-// import { ListProfileComponent } from './components/list-profile/list-profile.component';
-// import { DetailProfileComponent } from './components/detail-profile/detail-profile.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    // ListProfileComponent,
-    // DetailProfileComponent
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // ListProfileComponent,
-    // DetailProfileComponent
+    
 
   ],
   providers: [],
