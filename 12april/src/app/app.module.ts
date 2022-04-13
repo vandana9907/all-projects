297ae30b2d12786767f9f9485d2992b4973aef6b
vandana <vandanagurram07@gmail.com>
2022-04-13 18:38:28 +0530

    template form

--- 12april/src/app/app.module.ts
+++ 12april/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { ListCourseComponent } from './components/list-course/list-course.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    CoursesComponent,
+    ListCourseComponent,
     
     
   ],
--- assignment/src/app/app.module.ts
+++ assignment/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { CourseComponent } from './components/course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    CourseComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
