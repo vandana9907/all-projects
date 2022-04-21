c0fe338869a26a6e143528cafce98cc3f6008cd2
vandana9907 <vandanagurram07@gmail.com>
2022-04-21 20:18:47 +0530

    added support ticket in my sql connections

--- node-support/routes/users.js
+++ node-support/routes/users.js
@@ -10,5 +10,30 @@ res.send(resluts);
 });
 
 
+router.post('/profile', (req, res, next) => {
+ let {
+ username, email, password, first_name, last_name, job_title, salary, create_datetime
+ } = req.body
+
+ console.log(req.body);
+ var insertcommand = `INSERT INTO user_profile (username,email,password,first_name,last_name,job_title,salary,create_datetime) VALUES ('${username}','${email}','${password}','${first_name}','${last_name}','${job_title}','${salary}','${create_datetime}')`;
+
+dbConnection.query(insertcommand, (err, result) => {
+
+if (err) throw err;
+ res.send(result);
+  });
+
+})
+router.get('/profile', function (req, res, next) {
+
+    dbConnection.query('select * from user_profile', (error, results, fields) => {
+   
+    if (error) throw error;
+    res.send(results)
+   
+     })
+   
+   });
 
 module.exports = router;
